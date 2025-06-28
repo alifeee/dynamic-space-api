7461521faa799185c230f97c2fed80823abdd512
alifeee <alifeee.web@outlook.com>
2025-06-28 12:45:03 +0000

    fix open time

--- get_open-status.sh
+++ get_open-status.sh
@@ -15,7 +15,8 @@ fi
 echo "got ${result}" >> /dev/stderr
 
 state=$(echo "${result}" | csvtool namedcol "_value" - | head -n2 | tail -n1)
-modifiedtime=$(date --date=$(echo "${result}" | csvtool namedcol "_time" - | head -n2 | tail -n1) '+%s')
+modifiedtime=$(echo "${result}" | csvtool namedcol "_time" - | head -n2 | tail -n1)
+modifiedtime_unix=$(date --date="${modifiedtime}" '+%s')
 
 if [[ "${state}" == "OCCUPIED" ]]; then
   open="true"
@@ -25,14 +26,15 @@ fi
 
 jq -n \
   --arg open "${open}" \
-  --arg lastchange "${modifiedtime}" \
+	--arg modifiedtime "${modifiedtime}" \
+  --arg lastchange "${modifiedtime_unix}" \
   --arg iconopen "https://server.alifeee.net/static/images/sh-open.svg" \
   --arg iconclosed "https://server.alifeee.net/static/images/sh-open.svg" \
   '{
     "state": {
       "open": $open,
       "lastchange": $lastchange,
-      "message": "checked by pi",
+      "message": ("checked by pi at " + $modifiedtime + " UTC"),
       "icon": {
         "open": $iconopen,
         "closed": $iconclosed
